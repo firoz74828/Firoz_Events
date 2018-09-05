83869a6fe9602d97b620608a4ad5c5d18a02bd58
firoz74828 <firoz.shaikh@gridlogic.in>
2018-09-05 12:26:15 +0530

    changed wsgi.py

--- Events/wsgi.py
+++ Events/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 
 
 
@@ -18,5 +18,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Events.settings")
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/path/to/static/files')
-application.add_files('/path/to/more/static/files', prefix='more-files/')
+application = DjangoWhiteNoise(application)
