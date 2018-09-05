b9b1e13fcef9d7d5d4865d98265fc4919c0b018d
firoz74828 <firoz.shaikh@gridlogic.in>
2018-09-05 12:10:46 +0530

    changed white noise

--- Events/settings.py
+++ Events/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/static/media/'
